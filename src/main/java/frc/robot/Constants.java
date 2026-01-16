0bb1ba6798bec315a5ef2c14c5ed4e353dbcbafd
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2026-01-15 18:56:33 -0600

    Moved Constants.java to the correct place.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,17 +5,48 @@ import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import frc.robot.generated.TunerConstants;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Constants
 {
+    public static class AdvantageKit
+    {
+        public static final Mode SIM_MODE     = Mode.SIM;
+        public static final Mode CURRENT_MODE = RobotBase.isReal() ? Mode.REAL : SIM_MODE;
+
+        public static enum Mode
+        {
+            /** Running on a real robot. */
+            REAL,
+
+            /** Running a physics simulator. */
+            SIM,
+
+            /** Replaying from a log file. */
+            REPLAY
+        }
+    }
+
     public static class AIO
     {
+        public static final int FL_ENCODER = 0;
+        public static final int FR_ENCODER = 1;
+        public static final int BL_ENCODER = 2;
+        public static final int BR_ENCODER = 3;
     }
 
     public static class CAN
     {
         /* CAN IDs 1 through 13 are used by the drive subsystem and configured in TunerConstants */
-        public static final int INTAKE = 20;
+        public static final int FL_DRIVE = 1;
+        public static final int FR_DRIVE = 3;
+        public static final int BL_DRIVE = 5;
+        public static final int BR_DRIVE = 7;
+        public static final int FL_TURN  = 2;
+        public static final int FR_TURN  = 4;
+        public static final int BL_TURN  = 6;
+        public static final int BR_TURN  = 8;
+        public static final int INTAKE   = 67; 
     }
 
     public static class Drive
