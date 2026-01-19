e1b20f4b5a608765b4cddc26e34742a0bb0a2325
Collin McIntyre <crm11396@gmail.com>
2026-01-19 11:36:22 -0600

    Removed old Sim mode

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,21 +13,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Constants
 {
-    public static final Mode SIM_MODE     = Mode.SIM;
-    public static final Mode CURRENT_MODE = RobotBase.isReal() ? Mode.REAL : SIM_MODE;
-
-    public static enum Mode
-    {
-        /** Running on a real robot. */
-        REAL,
-
-        /** Running a physics simulator. */
-        SIM,
-
-        /** Replaying from a log file. */
-        REPLAY
-    }
-
+    
     public static class AIO
     {
         public static final int FL_ENCODER = 0;
