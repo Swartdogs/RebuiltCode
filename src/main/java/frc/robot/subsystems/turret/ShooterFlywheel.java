a83b1e4f19fd61a96869b263c22a68bdea8b9d96
Wool <woolstar@anywhere.com>
2026-01-19 17:37:53 -0600

    Closed loop control.
    Based on issue link.
    https://github.com/REVrobotics/REVLib-Examples/blob/main/Java/SPARK/Closed%20Loop%20Control/src/main/java/frc/robot/Robot.java

--- src/main/java/frc/robot/subsystems/turret/ShooterFlywheel.java
+++ src/main/java/frc/robot/subsystems/turret/ShooterFlywheel.java
@@ -1,12 +1,13 @@
 package frc.robot.subsystems.turret;
-import edu.wpi.first.math.controller.PIDController;
-import frc.robot.Constants.Flywheel;
-import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
-import frc.robot.Constants; //TODO: Update constants as needed.
+import frc.robot.Constants.Flywheel;
+
     /*Hardware
 The following motors and sensors will need to be added to the subsystem for controlling the hardware
 
@@ -18,68 +19,92 @@ Sensors
 Hood angle absolute encoder (1x sensor of undetermined type)
 Turret angle absolute encoder (1x sensor of undetermined type)
 Camera (1x Limelight) */
+
 public class ShooterFlywheel extends SubsystemBase
 {
-    private final ShooterFlywheelIO                 _io;
-   // private final ShooterFlywheelIOInputsAutoLogged _inputs                = new ShooterFlywheelIOInputsAutoLogged();
-    private double                                  _maxSpeed              = Constants.Flywheel.MAX_FLYWHEEL_SPEED * Constants.General.MAX_NEO_SPEED;
-    private double                                  _velocityRange         = Constants.Flywheel.VELOCITY_RANGE;
-    private Double                                  _upperVelocitySetpoint = null;
-    private Double                                  _lowerVelocitySetpoint = null;
-    private final PIDController _flyvolPID;
+    private final SparkMax                    _motor;
+    private final SparkClosedLoopController   _control ;
+    private final RelativeEncoder             _encoder;
 
-    public ShooterFlywheel(ShooterFlywheelIO flywheelIO)
-    {
-        _io = flywheelIO;
-        _flyvolPID = new PIDController(0.03, 0, 0);
-    }
-     private void  initPID( PIDController pid )
-    {
-        
-        pid.setTolerance(Flywheel.PIDTolerance);
-       //      pid.setIZone(0);
-         //   pid.setFF(0.00017);
-           // pid.setOutputRange(-1, 1);'''
-         
-        initPID( _flyvolPID ) ;
+    private double                  _velocity ;
+    private double                  _velocityTarget ;
+    private double                  _maxSpeed       = Constants.Flywheel.MAX_FLYWHEEL_SPEED * Constants.General.MAX_NEO_SPEED;
+    private double                  _velocityRange  = Constants.Flywheel.VELOCITY_RANGE;
 
+    private SparkMaxConfig    config()
+    {
+        SparkMaxConfig  val = new SparkMaxConfig() ;
+
+        val.encoder
+              .positionConversionFactor(1)
+              .velocityConversionFactor(1);
+
+        val.closedLoop
+              .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                // Set PID values for position control.
+                // We don't need to pass a closed loop slot, as it will default to slot 0.
+              .p(0.1)
+              .i(0)
+              .d(0)
+              .outputRange(-1, 1)
+                // Set PID values for velocity control in slot 1
+              .p(0.0001, ClosedLoopSlot.kSlot1)
+              .i(0, ClosedLoopSlot.kSlot1)
+              .d(0, ClosedLoopSlot.kSlot1)
+              .outputRange(-1, 1, ClosedLoopSlot.kSlot1)
+              .feedForward
+                // kV is now in Volts, so we multiply by the nominal voltage (12V)
+              .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
+
+        return val ;
     }
+
+    private void  initMotor( SparkMax motor )
+    {
+        motor.setCANTimeout(250);
+
+          // set PID config
+        motor.configure( config(), ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters ) ;
+
+        motor.setCANTimeout( 0 ) ;
+    }
+
+    public ShooterFlywheel()
+    {
+        _motor = new SparkMax(Constants.CAN.SHOOTERFLYWHEEL, MotorType.kBrushless ) ;
+        _control = _motor.getClosedLoopController() ;
+        _encoder = _motor.getEncoder() ;
+
+        initMotor( _motor ) ;
+    }
+
     @Override
     public void periodic()
     {
-        // _io.updateInputs(_inputs);
-        // Logger.processInputs("Shooter/Flywheel", _inputs);
+        _velocity = _encoder.getVelocity() ;
+        // updateInputs(_inputs);
     }
 
-    public void setUpperVelocity(double upperVelocity)
+    public boolean  atSpeed()
     {
-        _upperVelocitySetpoint = MathUtil.clamp(upperVelocity, 0, _maxSpeed);
-        Logger.recordOutput("Shooter/Flywheel/UpperVelocitySetpoint", _upperVelocitySetpoint);
-        _io.setVelocity(_upperVelocitySetpoint);
+        return (_velocityTarget > 0. ) && ( _velocity > _velocityTarget ) ;
+    }
+
+    public void setVelocity(double velocity)
+    {
+        _velocityTarget = MathUtil.clamp(velocity, 0, _maxSpeed);
+        // Logger.recordOutput("Shooter/Flywheel/UpperVelocitySetpoint", _velocityTarget);
+        _control.setSetpoint(_velocityTarget, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
     }
 
     public void stop()
     {
-        _io.stop() ;
+        setVelocity( 0. ) ;
     }
 
     public void setMaxSpeed(double maxFlywheelSpeed)
     {
         _maxSpeed = maxFlywheelSpeed * Constants.General.MAX_NEO_SPEED;
     }
-
-    public boolean atSpeed()
-    {
-        return _upperVelocitySetpoint != null && Math.abs(_upperVelocitySetpoint - _io.getVelocity()) <= _upperVelocitySetpoint * _velocityRange;
-    }
-
-    public boolean isShooting()
-    {
-        return _upperVelocitySetpoint != null && _lowerVelocitySetpoint != null && _upperVelocitySetpoint > 0 && _lowerVelocitySetpoint > 0;
-    }
-
-    public void setVelocityRange(double velocityRange)
-    {
-        _velocityRange = velocityRange;
-    }
 }
+
