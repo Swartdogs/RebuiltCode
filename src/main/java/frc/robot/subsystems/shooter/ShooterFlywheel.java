c58a5b2701037551a05b0151f44ba9bcbb370ddb
Collin McIntyre <crm11396@gmail.com>
2026-01-20 20:14:31 -0600

    Shooter base (#18) skeleton
    * Initial flywheel function
    
    Relocate PID
    
    * collapse into single component
    
    * Closed loop control.
    
    Based on issue link.
    https://github.com/REVrobotics/REVLib-Examples/blob/main/Java/SPARK/Closed%20Loop%20Control/src/main/java/frc/robot/Robot.java
    
    * Created new files with the turret folder and which has been renamed to shooter.
    
    * Setup current Shooter enum, may need changing in the future.
    
    * This is the shooter skeleton
    
    * cleanup of uneeded files at collin,s command
    
    ---------
    
    Co-authored-by: Wool <woolstar@anywhere.com>

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterFlywheel.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/*
+ * Hardware The following motors and sensors will need to be added to the
+ * subsystem for controlling the hardware Motors Flywheel (2x NEO Vortex motors
+ * driven by SPARK Flex motor controllers) Hood (1x Motor of undetermined type)
+ * Turret (1x Kraken x44 driven by a TalonFX motor controller) Sensors Hood
+ * angle absolute encoder (1x sensor of undetermined type) Turret angle absolute
+ * encoder (1x sensor of undetermined type) Camera (1x Limelight)
+ */
+
+public class ShooterFlywheel extends SubsystemBase
+{
+
+}
