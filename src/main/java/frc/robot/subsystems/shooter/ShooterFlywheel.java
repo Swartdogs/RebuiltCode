5ea0f2d5301f1134d79fe8501c514f7b94e2f86e
Collin McIntyre <crm11396@gmail.com>
2026-01-22 19:33:38 -0600

    Removed initmotor function, moved into constructor. Configured ShooterFlywheel for 2 motors, with one set as a follower.

--- src/main/java/frc/robot/subsystems/shooter/ShooterFlywheel.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterFlywheel.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.*;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
@@ -23,49 +25,50 @@ import frc.robot.Constants;
 
 public class ShooterFlywheel extends SubsystemBase
 {
-    private final SparkFlex                 _motor;
-    private final SparkClosedLoopController _control;
+    private final SparkFlex                 _leadMotor;
+    private final SparkFlex                 _followMotor;
+    private final SparkClosedLoopController _closedLoopControl;
     private final RelativeEncoder           _encoder;
     private double                          _velocity;
     private double                          _velocityTarget;
-    private double                          _maxSpeed      = Constants.Shooter.MAX_FLYWHEEL_SPEED * Constants.General.MAX_NEO_SPEED;
-    private double                          _velocityRange = Constants.Shooter.VELOCITY_RANGE;
-
-    private SparkFlexConfig config()
-    {
-        SparkFlexConfig val = new SparkFlexConfig();
-
-        val.encoder.positionConversionFactor(1).velocityConversionFactor(1);
-
-        val.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                // Set PID values for position control.
-                // We don't need to pass a closed loop slot, as it will default to slot 0.
-                .p(0.1).i(0).d(0).outputRange(-1, 1)
-                // Set PID values for velocity control in slot 1
-                .p(0.0001, ClosedLoopSlot.kSlot1).i(0, ClosedLoopSlot.kSlot1).d(0, ClosedLoopSlot.kSlot1).outputRange(-1, 1, ClosedLoopSlot.kSlot1).feedForward
-                        // kV is now in Volts, so we multiply by the nominal voltage (12V)
-                        .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
-
-        return val;
-    }
-
-    private void initMotor(SparkFlex motor)
-    {
-        motor.setCANTimeout(250);
-
-        // set PID config
-        motor.configure(config(), ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-
-        motor.setCANTimeout(0);
-    }
+    private double                          _maxSpeed = Constants.Shooter.MAX_FLYWHEEL_SPEED * Constants.General.MAX_NEO_SPEED;
 
     public ShooterFlywheel()
     {
-        _motor   = new SparkFlex(Constants.CAN.SHOOTERFLYWHEEL, MotorType.kBrushless);
-        _control = _motor.getClosedLoopController();
-        _encoder = _motor.getEncoder();
+        if (RobotBase.isSimulation())
+        {
+            _leadMotor         = null;
+            _followMotor       = null;
+            _closedLoopControl = null;
+            _encoder           = null;
+            return;
+        }
+        _leadMotor   = new SparkFlex(Constants.CAN.FLYWHEEL_LEAD, MotorType.kBrushless);
+        _followMotor = new SparkFlex(Constants.CAN.FLYWHEEL_FOLLOW, MotorType.kBrushless);
+        _leadMotor.setCANTimeout(250);
 
-        initMotor(_motor);
+        var config = new SparkFlexConfig();
+        config.inverted(false).idleMode(IdleMode.kCoast).smartCurrentLimit(Constants.Shooter.FLYWHEEL_CURRENT_LIMIT).voltageCompensation(Constants.General.MOTOR_VOLTAGE);
+        config.encoder.positionConversionFactor(1).velocityConversionFactor(1);
+        config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                // Set PID values for position control.
+                // We don't need to pass a closed loop slot, as it will default to slot 0.
+                .p(0.1).d(0).outputRange(-1, 1)
+                // Set PID values for velocity control in slot 1
+                .p(Constants.Shooter.FLYWHEEL_KP, ClosedLoopSlot.kSlot1).d(Constants.Shooter.FLYWHEEL_KD, ClosedLoopSlot.kSlot1).outputRange(-1, 1, ClosedLoopSlot.kSlot1).feedForward
+                        // kV is now in Volts, so we multiply by the nominal voltage (12V)
+                        .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
+        _leadMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kCoast).smartCurrentLimit(Constants.Shooter.FLYWHEEL_CURRENT_LIMIT).voltageCompensation(Constants.General.MOTOR_VOLTAGE).follow(_leadMotor, true);
+        _followMotor.setCANTimeout(250);
+        _followMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        _closedLoopControl = _leadMotor.getClosedLoopController();
+        _encoder           = _leadMotor.getEncoder();
+        _leadMotor.setCANTimeout(0);
+        _followMotor.setCANTimeout(0);
     }
 
     @Override
@@ -85,7 +88,7 @@ public class ShooterFlywheel extends SubsystemBase
         _velocityTarget = MathUtil.clamp(velocity, 0, _maxSpeed);
         // Logger.recordOutput("Shooter/Flywheel/UpperVelocitySetpoint",
         // _velocityTarget);
-        _control.setSetpoint(_velocityTarget, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
+        _closedLoopControl.setSetpoint(_velocityTarget, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
     }
 
     public void stop()
