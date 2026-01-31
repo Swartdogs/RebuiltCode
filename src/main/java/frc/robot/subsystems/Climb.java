8dee03c4a61f78890f45a2d396087aba09a99d4b
SAKETH11111 <sakethbaddam10@gmail.com>
2026-01-31 16:57:45 -0600

    add motor configurations (might change), update extension logic, and implement rotation control for climber

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,74 +1,158 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.mechanisms.swerve.LegacySwerveRequest.Idle;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants;
 import frc.robot.Constants.ClimberConstants;
 
-public class Climb extends SubsystemBase 
+@Logged
+public class Climb extends SubsystemBase
 {
     public enum ClimbLevel
     {
-        L1, L3; // TODO
+        L1, L3
     }
-    
+
     public enum ExtensionState
     {
         Idle, Extending, Retracting
     }
 
+    private final TalonFX  _extendMotor;
+    private final TalonFX  _rotateMotor;
     @Logged
-    private double _currentRotation; 
+    private double         _currentRotation  = 0.0; // TODO: replace with encoder position
     @Logged
-    private double _currentExtension; 
+    private double         _currentExtension = 0.0; // TODO: replace with encoder position
     @Logged
-    private ExtensionState _extenstionState; 
+    private ExtensionState _extensionState   = ExtensionState.Idle;
     @Logged
-    private boolean _extended; 
+    private double         _extendOutput     = 0.0;
     @Logged
-    private boolean _retracted; 
+    private double         _rotateOutput     = 0.0;
+    @Logged
+    private double         _rotationTarget   = 0.0;
+    @Logged
+    private boolean        _extended         = false;
+    @Logged
+    private boolean        _retracted        = true;
 
-    public Climb() 
+    // TODO: Replace with real limit switch inputs.
+    private boolean _outSwitch = false;
+    private boolean _inSwitch  = true;
+
+    public Climb()
     {
-        _extenstionState = ExtensionState.Idle; 
-        _currentRotation = 0.0; 
-        _currentExtension = 0.0; 
-        _extended = false; 
-        _retracted = true; 
+        _extendMotor = new TalonFX(Constants.CAN.CLIMBER_EXTEND);
+        _rotateMotor = new TalonFX(Constants.CAN.CLIMBER_ROTATE);
+
+        var extendConfig = new MotorOutputConfigs();
+        extendConfig.NeutralMode = NeutralModeValue.Brake;
+        extendConfig.Inverted    = InvertedValue.Clockwise_Positive; // TODO: verify extend motor inversion
+        _extendMotor.getConfigurator().apply(extendConfig);
+
+        var rotateConfig = new MotorOutputConfigs();
+        rotateConfig.NeutralMode = NeutralModeValue.Brake;
+        rotateConfig.Inverted    = InvertedValue.CounterClockwise_Positive; // TODO: verify rotate motor inversion
+        _rotateMotor.getConfigurator().apply(rotateConfig);
     }
 
-    public void periodic() 
+    @Override
+    public void periodic()
     {
-        updateExtension();   
-        _extended = isExtended(); 
-        _retracted = isRetracted(); 
+        // TODO: Read sensors/encoders to update _currentRotation/_currentExtension.
+        updateExtensionStateFromLimits();
+        updateExtension();
+        updateRotation();
+        applyOutputs();
+    }
 
-        if (_extended || _retracted) // hopefully both aren't true at the same time. if so, we use xor (^)
+    private void updateExtension()
+    {
+        switch (_extensionState)
         {
-            _extenstionState = ExtensionState.Idle; 
-        }
-    }
-
-    public void updateExtension()
-    {
-        switch (_extenstionState) {
             case Idle:
-                // tell the motor to stop
+                _extendOutput = 0.0;
                 break;
 
             case Extending:
-                // tell the motor to extend 
-                // update _currentExtension
-                break; 
-            
+                _extendOutput = ClimberConstants.EXTEND_OUTPUT;
+                break;
+
             case Retracting:
-                // tell the motor to retract 
-                // update _currentExtension 
-                break; 
-        
+                _extendOutput = -ClimberConstants.EXTEND_OUTPUT;
+                break;
+
+            default:
+                _extendOutput = 0.0;
+                break;
+        }
+    }
+
+    private void updateExtensionStateFromLimits()
+    {
+        boolean atExtendLimit  = _outSwitch;
+        boolean atRetractLimit = _inSwitch;
+
+        if (atExtendLimit)
+        {
+            _extended  = true;
+            _retracted = false;
+
+            if (_extensionState == ExtensionState.Extending)
+            {
+                _extensionState = ExtensionState.Idle;
+            }
+        }
+
+        if (atRetractLimit)
+        {
+            _retracted = true;
+            _extended  = false;
+
+            if (_extensionState == ExtensionState.Retracting)
+            {
+                _extensionState = ExtensionState.Idle;
+            }
+        }
+    }
+
+    private void updateRotation()
+    {
+        if (!_extended)
+        {
+            _rotateOutput = 0.0;
+            return;
+        }
+
+        if (Math.abs(_currentRotation - _rotationTarget) <= ClimberConstants.ROTATION_TOLERANCE)
+        {
+            _rotateOutput = 0.0;
+            return;
+        }
+
+        _rotateOutput = (_rotationTarget > _currentRotation) ? ClimberConstants.ROTATE_OUTPUT : -ClimberConstants.ROTATE_OUTPUT;
+    }
+
+    public void setRotationTarget(ClimbLevel level)
+    {
+        switch (level)
+        {
+            case L1:
+                _rotationTarget = ClimberConstants.L1_ROTATION;
+                break;
+
+            case L3:
+                _rotationTarget = ClimberConstants.L3_ROTATION;
+                break;
+
             default:
                 break;
         }
@@ -76,58 +160,33 @@ public class Climb extends SubsystemBase
 
     public void setExtensionState(ExtensionState state)
     {
-        switch (_extenstionState) {
+        switch (_extensionState)
+        {
             case Idle:
-                if (state == ExtensionState.Extending && !_extended) 
+                if (state == ExtensionState.Extending && !_extended)
                 {
-                    _extenstionState = state; 
+                    _extensionState = state;
                 }
                 else if (state == ExtensionState.Retracting && !_retracted)
                 {
-                    _extenstionState = state; 
+                    _extensionState = state;
                 }
                 break;
 
             case Extending:
-                if (state == ExtensionState.Idle && _extended) 
+                if (state == ExtensionState.Idle || state == ExtensionState.Retracting)
                 {
-                    _extenstionState = state; 
-                }
-                else if (state == ExtensionState.Retracting)
-                {
-                    _extenstionState = state; 
+                    _extensionState = state;
                 }
                 break;
 
             case Retracting:
-                if (state == ExtensionState.Idle && _retracted) 
-                    {
-                        _extenstionState = state; 
-                    }
-                    else if (state == ExtensionState.Extending)
-                    {
-                        _extenstionState = state; 
-                    }
-                break;
-        
-            default:
-                break;
-        }
-    }
-
-    public void rotateClimber(ClimbLevel level)
-    {
-        // TODO 
-        switch (level) 
-        {
-            case L1:
-                // turn using ClimberConstants.L1_ROTATION
+                if (state == ExtensionState.Idle || state == ExtensionState.Extending)
+                {
+                    _extensionState = state;
+                }
                 break;
 
-            case L3:
-                // turn using ClimberConstants.L3_ROTATION
-                break;
-        
             default:
                 break;
         }
@@ -135,23 +194,47 @@ public class Climb extends SubsystemBase
 
     public Command getExtendCmd()
     {
-        return runOnce(); // TODO 
+        return runOnce(() -> setExtensionState(ExtensionState.Extending));
+    }
+
+    public Command getRetractCmd()
+    {
+        return runOnce(() -> setExtensionState(ExtensionState.Retracting));
     }
 
     public Command getRotateCmd(ClimbLevel level)
     {
-        return runOnce(
-            () -> rotateClimber(level)
-        ); // TODO 
+        return runOnce(() -> setRotationTarget(level));
     }
 
-    public boolean isExtended() 
+    public Command getStopCmd()
     {
-        return _currentExtension >= ClimberConstants.EXTENSION_THRESHOLD;
+        return runOnce(() -> setExtensionState(ExtensionState.Idle));
     }
 
-    public boolean isRetracted() 
+    public void setOutSwitch(boolean outSwitch)
     {
-        return _currentExtension <= ClimberConstants.RETRACTION_THRESHOLD;
+        _outSwitch = outSwitch;
+    }
+
+    public void setInSwitch(boolean inSwitch)
+    {
+        _inSwitch = inSwitch;
+    }
+
+    public boolean isExtended()
+    {
+        return _outSwitch;
+    }
+
+    public boolean isRetracted()
+    {
+        return _inSwitch;
+    }
+
+    private void applyOutputs()
+    {
+        _extendMotor.set(_extendOutput);
+        _rotateMotor.set(_rotateOutput);
     }
 }
