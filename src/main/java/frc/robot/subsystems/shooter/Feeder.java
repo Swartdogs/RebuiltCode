3fac383ee488ce2f96f1c9abfd4d7556aaa34e83
Collin McIntyre <crm11396@gmail.com>
2026-02-05 19:39:56 -0600

    Made Feeder sim

--- src/main/java/frc/robot/subsystems/shooter/Feeder.java
+++ src/main/java/frc/robot/subsystems/shooter/Feeder.java
@@ -5,21 +5,27 @@ import static edu.wpi.first.units.Units.Volts;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkSim;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
+//yello
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Voltage;
-
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANConstants;
 import frc.robot.Constants.GeneralConstants;
 import frc.robot.Constants.ShooterConstants;
 
 @Logged
-public class Feeder
+public class Feeder extends SubsystemBase
 {
     private final SparkFlex _feederMotor;
+    private final SparkSim  _feederMotorSim;
+    @Logged
     private Voltage         _feederMotorVoltage = Volts.of(0.0);
 
     public Feeder()
@@ -28,8 +34,16 @@ public class Feeder
 
         var config = new SparkFlexConfig();
         config.inverted(false).idleMode(IdleMode.kCoast).smartCurrentLimit(ShooterConstants.FEEDER_CURRENT_LIMIT).voltageCompensation(GeneralConstants.MOTOR_VOLTAGE);
-
+//hi
         _feederMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        if (RobotBase.isReal())
+        {
+            _feederMotorSim = null;
+        }
+        else
+        {
+            _feederMotorSim = new SparkSim(_feederMotor, DCMotor.getNeoVortex(1));
+        }
     }
 
     public void periodic()
@@ -37,6 +51,12 @@ public class Feeder
         _feederMotorVoltage = Volts.of(_feederMotor.getAppliedOutput() * _feederMotor.getBusVoltage());
     }
 
+    public void simulationPeriodic()
+    {
+        if (_feederMotorSim == null) return;
+        _feederMotorSim.iterate(0., RoboRioSim.getVInVoltage(), GeneralConstants.LOOP_PERIOD_SECS);
+    }
+//hello
     public void set(boolean on)
     {
         _feederMotor.setVoltage(on ? ShooterConstants.FEEDER_VOLTAGE : 0.0);
