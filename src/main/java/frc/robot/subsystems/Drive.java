44ec5b2a5873fb404e60bb4284f228aefcbadd64
Collin McIntyre <crm11396@gmail.com>
2026-02-14 02:04:54 -0600

    fix build error and update readme with proper formatting

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -341,8 +341,8 @@ public class Drive extends TunerSwerveDrivetrain implements Subsystem
                 _poseEstimatorLeft  = _limelightLeft.createPoseEstimator(EstimationMode.MEGATAG2);
                 _poseEstimatorRight = _limelightRight.createPoseEstimator(EstimationMode.MEGATAG2);
 
-                _limelightLeft.getSettings().withCameraOffset(Constants.Vision.LEFT_CAMERA_OFFSET).save();
-                _limelightRight.getSettings().withCameraOffset(Constants.Vision.RIGHT_CAMERA_OFFSET).save();
+                _limelightLeft.getSettings().withCameraOffset(VisionConstants.LEFT_CAMERA_OFFSET).save();
+                _limelightRight.getSettings().withCameraOffset(VisionConstants.RIGHT_CAMERA_OFFSET).save();
             }
             else
             {
