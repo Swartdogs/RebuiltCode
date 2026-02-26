a25d017b0e04e4c3371f1b980b775f9e43a1762f
SAKETH11111 <sakethbaddam10@gmail.com>
2026-02-25 22:36:35 -0600

    Update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -36,7 +37,7 @@ public class RobotContainer
     private final SwerveRequest.SwerveDriveBrake brake       = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt    point       = new SwerveRequest.PointWheelsAt();
     private final Telemetry                      _logger     = new Telemetry(DriveConstants.MAX_SPEED.in(MetersPerSecond));
-    private final CommandXboxController          _driver     = new CommandXboxController(0);
+    private final CommandJoystick                _driver     = new CommandJoystick(0);
     private final CommandXboxController          _operator   = new CommandXboxController(1);
     private final Drive                          _drivetrain = TunerConstants.createDrivetrain();
     private final Intake                         _intake     = new Intake();
@@ -57,9 +58,9 @@ public class RobotContainer
         _drivetrain.setDefaultCommand(
                 // Drivetrain will execute this command periodically
                 _drivetrain.applyRequest(
-                        () -> drive.withVelocityX(MeasureUtil.applyDeadband(DriveConstants.MAX_SPEED.times(Value.of(-_driver.getLeftY())), DriveConstants.DEADBAND))// Drive forward with negative Y (forward)
-                                .withVelocityY(MeasureUtil.applyDeadband(DriveConstants.MAX_SPEED.times(Value.of(-_driver.getLeftX())), DriveConstants.DEADBAND)) // Drive left with negative X (left)
-                                .withRotationalRate(MeasureUtil.applyDeadband(DriveConstants.MAX_ANGULAR_RATE.times(Value.of(-_driver.getRightX())), DriveConstants.DEADBAND)) // Drive counterclockwise with negative X (left)
+                        () -> drive.withVelocityX(MeasureUtil.applyDeadband(DriveConstants.MAX_SPEED.times(Value.of(-_driver.getY())), DriveConstants.DEADBAND))// Drive forward with negative Y (forward)
+                                .withVelocityY(MeasureUtil.applyDeadband(DriveConstants.MAX_SPEED.times(Value.of(-_driver.getX())), DriveConstants.DEADBAND)) // Drive left with negative X (left)
+                                .withRotationalRate(MeasureUtil.applyDeadband(DriveConstants.MAX_ANGULAR_RATE.times(Value.of(-_driver.getTwist())), DriveConstants.DEADBAND)) // Drive counterclockwise with negative X (left)
                 )
         );
 
@@ -69,15 +70,15 @@ public class RobotContainer
 
         RobotModeTriggers.disabled().whileTrue(_drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-        _driver.a().whileTrue(_drivetrain.applyRequest(() -> brake));
-        _driver.b().whileTrue(_drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-_driver.getLeftY(), -_driver.getLeftX()))));
+        _driver.button(1).whileTrue(_drivetrain.applyRequest(() -> brake));
+        _driver.button(2).whileTrue(_drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-_driver.getY(), -_driver.getX()))));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        _driver.x().whileTrue(_drivetrain.sysIdDynamic(Direction.kForward));
-        _driver.y().whileTrue(_drivetrain.sysIdDynamic(Direction.kReverse));
-        _driver.leftBumper().whileTrue(_drivetrain.sysIdQuasistatic(Direction.kForward));
-        _driver.rightBumper().whileTrue(_drivetrain.sysIdQuasistatic(Direction.kReverse));
+        _driver.button(3).whileTrue(_drivetrain.sysIdDynamic(Direction.kForward));
+        _driver.button(4).whileTrue(_drivetrain.sysIdDynamic(Direction.kReverse));
+        _driver.button(5).whileTrue(_drivetrain.sysIdQuasistatic(Direction.kForward));
+        _driver.button(6).whileTrue(_drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         _operator.a().onTrue(Commands.runOnce(() -> _intake.extend(!_intake.isExtended()), _intake));
         _operator.x().onTrue(_intake.startRollers());
